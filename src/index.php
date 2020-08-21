f56a2023d557d76e729eb8e287a068d131e03ced
Alexander Romberg <adze@gmx.ch>
2020-08-21 12:07:15 +0200

    added basic content

--- src/index.php
+++ src/index.php
@@ -1,1 +1,27 @@
+<!DOCTYPE html>
 
+<head>
+    <!-- Settings -->
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+
+    <!-- Stylesheets -->
+    <link rel="stylesheet" type="text/css" href="css/general.css">
+    <link rel="stylesheet" type="text/css" href="css/<?php echo(basename($_SERVER['SCRIPT_NAME'], ".php")) ?>.css">
+
+    <!-- Favicons -->
+</head>
+
+<body>
+    <header></header>
+    <nav></nav>
+    <main></main>
+    <footer></footer>
+</body>
