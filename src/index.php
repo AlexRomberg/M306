324d226470920f55800ff8f116858bba0a44a51a
ALRND <dackralp@gmail.com>
2020-08-28 11:25:46 +0200

    Changed Title

--- src/index.php
+++ src/index.php
@@ -7,7 +7,7 @@
     <meta name="keywords" content="">
     <meta name="author" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <title>Nutzungsfläche Thurgau</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
