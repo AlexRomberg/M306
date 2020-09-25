eb75da3db6b3360b7b669a4d0c64c6d74bdf0bf0
ALRND <dackralp@gmail.com>
2020-09-25 12:32:57 +0200

    added CSS Grid

--- src/index.php
+++ src/index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="Website die die Kantonale Flächen des Thurgaus grafisch darstellt" content="">
     <meta name="keywords" content="">
-    <meta name="Mica, Alexander, Luca" content="">
+    <meta name="Author" content="Mica, Alexander, Luca">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nutzungsfläche Thurgau</title>
 
@@ -14,26 +14,48 @@
 
     <!-- Stylesheets -->
     <link rel="stylesheet" type="text/css" href="css/general.css">
-    <link rel="stylesheet" type="text/css" href="css/<?php echo(basename($_SERVER['SCRIPT_NAME'], ".php")) ?>.css">
+    <link rel="stylesheet" type="text/css" href="css/<?php echo (basename($_SERVER['SCRIPT_NAME'], ".php")) ?>.css">
 
     <!-- Favicons -->
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-<link rel="manifest" href="/site.webmanifest">
-<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
-<meta name="msapplication-TileColor" content="#ffc40d">
-<meta name="theme-color" content="#ffffff">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#ffc40d">
+    <meta name="theme-color" content="#ffffff">
 
 </head>
 
 <body>
+    <div class="grid-container">
+        <div class="item1">
+            Nutzungsfläche Kanton Thurgau
+        </div>
+
+        <div class="item2">
+            Map
+        </div>
+
+        <div class="item3">
+            Filter
+        </div>
+
+        <div class="item4">
+            Description
+        </div>
+
+        <div class="item5">
+            copyright
+        </div>
+    </div>
+
     <header></header>
     <nav></nav>
     <main></main>
     <footer></footer>
 
-<!-- Script -->
-<script src="js/index.js"></script>
+    <!-- Script -->
+    <script src="js/index.js"></script>
 
 </body>
