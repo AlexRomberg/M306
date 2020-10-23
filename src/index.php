03565729361de28d73b0b2b1e3d4430e961eb93d
ALRND <dackralp@gmail.com>
2020-10-23 10:51:47 +0200

    added flexbox gui

--- src/index.php
+++ src/index.php
@@ -28,32 +28,58 @@
 </head>
 
 <body>
-    <div class="grid-container">
-        <div class="item1">
-            Nutzungsfläche Kanton Thurgau
-        </div>
 
-        <div class="item2">
-            Map
-        </div>
+    <!-- Note -->
+    <div style="background:yellow;padding:5px">
+        <h4 style="text-align:center">Resize the browser window to see the responsive effect.</h4>
+    </div>
 
-        <div class="item3">
-            Filter
-        </div>
+    <!-- Header -->
+    <div class="header">
+        <h1>My Website</h1>
+        <p>With a <b>flexible</b> layout.</p>
+    </div>
 
-        <div class="item4">
-            Description
-        </div>
+    <!-- Navigation Bar -->
+    <div class="navbar">
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+    </div>
 
-        <div class="item5">
-            copyright
+    <!-- The flexible grid (content) -->
+    <div class="row">
+        <div class="side">
+            <h2>About Me</h2>
+            <h5>Photo of me:</h5>
+            <div class="fakeimg" style="height:200px;">Image</div>
+            <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
+            <h3>More Text</h3>
+            <p>Lorem ipsum dolor sit ame.</p>
+            <div class="fakeimg" style="height:60px;">Image</div><br>
+            <div class="fakeimg" style="height:60px;">Image</div><br>
+            <div class="fakeimg" style="height:60px;">Image</div>
+        </div>
+        <div class="main">
+            <h2>TITLE HEADING</h2>
+            <h5>Title description, Dec 7, 2017</h5>
+            <div class="fakeimg" style="height:200px;">Image</div>
+            <p>Some text..</p>
+            <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+            <br>
+            <h2>TITLE HEADING</h2>
+            <h5>Title description, Sep 2, 2017</h5>
+            <div class="fakeimg" style="height:200px;">Image</div>
+            <p>Some text..</p>
+            <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
         </div>
     </div>
 
-    <header></header>
-    <nav></nav>
-    <main></main>
-    <footer></footer>
+    <!-- Footer -->
+    <div class="footer">
+        <h2>Footer</h2>
+    </div>
 
     <!-- Script -->
     <script src="js/index.js"></script>
