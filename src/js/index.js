3fd269415b7789527a276cafb7bb44bc1a68c461
Alexander Romberg <adze@gmx.ch>
2020-10-23 11:56:53 +0200

    added working Map layering

--- src/js/index.js
+++ src/js/index.js
@@ -6,6 +6,7 @@
 // var wmsLayer = L.tileLayer.wms('', wmsOptions).addTo(map);
 
 var mymap = L.map('map').setView([47.550, 9.060], 10);
+let layers = new Array();
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
     maxZoom: 18,
@@ -13,8 +14,31 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     tileSize: 512,
     zoomOffset: -1
 }).addTo(mymap);
-L.tileLayer.wms('https://ows.geo.tg.ch/geofy_access_proxy/landwirtschaft_kulturflaechen?', {
-    layers: 'Nutzungsflaechen',
+layers.push(L.tileLayer.wms('https://ows.geo.tg.ch/geofy_access_proxy/landwirtschaft_kulturflaechen?', {
+    layers: 'nutzungsflaechen_flaechen_tg',
     format: 'image/png',
     transparent: true
-}).addTo(mymap);
+}));
+layers.push(L.tileLayer.wms('https://ows.geo.tg.ch/geofy_access_proxy/landwirtschaft_kulturflaechen?', {
+    layers: 'nutzungsflaechen_',
+    format: 'image/png',
+    transparent: true
+}));
+layers.push(L.tileLayer.wms('https://ows.geo.tg.ch/geofy_access_proxy/landwirtschaft_kulturflaechen?', {
+    layers: 'nutzungsflaechen_hauptkulturen',
+    format: 'image/png',
+    transparent: true
+}));
+layers.push(L.tileLayer.wms('https://ows.geo.tg.ch/geofy_access_proxy/landwirtschaft_kulturflaechen?', {
+    layers: 'nutzungsflaechen_hauptkulturen_textur',
+    format: 'image/png',
+    transparent: true
+}));
+
+layers.forEach(layer => {
+    layer.addTo(mymap);
+});
+
+// layers.forEach(layer => {
+//     layer.removeFrom(mymap);
+// });
