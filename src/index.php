ff54dad739f60d2cea74c2468cfbea01d2cf9e0f
ALRND <dackralp@gmail.com>
2020-10-23 11:46:21 +0200

    changed GUI design, layout

--- src/index.php
+++ src/index.php
@@ -13,66 +13,49 @@
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
 
     <!-- Stylesheets -->
-    <link rel="stylesheet" type="text/css" href="css/general.css">
-    <link rel="stylesheet" type="text/css" href="css/<?php echo (basename($_SERVER['SCRIPT_NAME'], ".php")) ?>.css">
+    <link rel="stylesheet" href="css/<?php echo (basename($_SERVER['SCRIPT_NAME'], ".php")) ?>.css">
+    <link rel="stylesheet" href="css/general.css">
+
 
     <!-- Favicons -->
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-    <link rel="manifest" href="/site.webmanifest">
-    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+    <link rel="manifest" href="favicon/site.webmanifest">
+    <link rel="mask-icon" href="favicon/safari-pinned-tab.svg" color="#5bbad5">
     <meta name="msapplication-TileColor" content="#ffc40d">
     <meta name="theme-color" content="#ffffff">
 
 </head>
 
 <body>
-
-    <!-- Note -->
-    <div style="background:yellow;padding:5px">
-        <h4 style="text-align:center">Resize the browser window to see the responsive effect.</h4>
-    </div>
-
     <!-- Header -->
     <div class="header">
-        <h1>My Website</h1>
-        <p>With a <b>flexible</b> layout.</p>
+        <h1>Nutzungsflächer Thurgau</h1>
+        <p>Logo</p>
     </div>
 
     <!-- Navigation Bar -->
     <div class="navbar">
-        <a href="#">Link</a>
-        <a href="#">Link</a>
-        <a href="#">Link</a>
-        <a href="#">Link</a>
+        <a href="#">Map</a>
+
+
     </div>
 
     <!-- The flexible grid (content) -->
     <div class="row">
         <div class="side">
-            <h2>About Me</h2>
-            <h5>Photo of me:</h5>
-            <div class="fakeimg" style="height:200px;">Image</div>
-            <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
-            <h3>More Text</h3>
+            <h2>Map</h2>
+            <div class="map" style="height:200px;">add Map Here</div>
             <p>Lorem ipsum dolor sit ame.</p>
-            <div class="fakeimg" style="height:60px;">Image</div><br>
-            <div class="fakeimg" style="height:60px;">Image</div><br>
-            <div class="fakeimg" style="height:60px;">Image</div>
+
         </div>
         <div class="main">
-            <h2>TITLE HEADING</h2>
-            <h5>Title description, Dec 7, 2017</h5>
-            <div class="fakeimg" style="height:200px;">Image</div>
-            <p>Some text..</p>
-            <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+            <h2>Filter</h2>
+            <div class="fakeimg" style="height:200px;">Filter</div>
             <br>
-            <h2>TITLE HEADING</h2>
-            <h5>Title description, Sep 2, 2017</h5>
-            <div class="fakeimg" style="height:200px;">Image</div>
-            <p>Some text..</p>
-            <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+            <h2>Legend</h2>
+            <div class="fakeimg" style="height:200px;">Legend</div>
         </div>
     </div>
 
@@ -80,6 +63,7 @@
     <div class="footer">
         <h2>Footer</h2>
     </div>
+
 
     <!-- Script -->
     <script src="js/index.js"></script>
